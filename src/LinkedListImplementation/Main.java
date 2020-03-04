070dc525cab893afcfc318122e64f157e81f3a81
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-05 08:34:39 +1400

    Nodes are created in linked list

--- src/LinkedListImplementation/Main.java
+++ src/LinkedListImplementation/Main.java
@@ -1,5 +1,12 @@
 package LinkedListImplementation;
 
 public class Main {
+    public static void main(String[] args) {
+        LinkedListOperations linkedListOperations = new LinkedListOperations();
+        linkedListOperations.head = new Node(19);
+        Node node1 = new Node(45);
+        Node node2 = new Node(75);
+        Node node3 = new Node(12);
 
+    }
 }
