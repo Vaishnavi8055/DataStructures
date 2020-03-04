f7e2b994fd426903534967cd94dc13e09899fe7b
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-05 08:06:58 +1400

    getter and setter methods of declared fields

--- src/LinkedListImplementation/Node.java
+++ src/LinkedListImplementation/Node.java
@@ -3,8 +3,24 @@ package LinkedListImplementation;
 public class Node {
     private int data;
     private Node nextRef;
+
     public Node(int data) {
         this.data = data;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNextRef() {
+        return nextRef;
+    }
+
+    public void setNextRef(Node nextRef) {
+        this.nextRef = nextRef;
+    }
 }
