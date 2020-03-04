c7a9e0399344855a233f28ce1484d637b00b509c
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-05 08:17:24 +1400

    Method to insert a Node at first

--- src/LinkedListImplementation/LinkedListOperations.java
+++ src/LinkedListImplementation/LinkedListOperations.java
@@ -2,4 +2,17 @@ package LinkedListImplementation;
 
 public class LinkedListOperations {
   Node head;
+
+
+  public void insertAtBeginning(int new_data) {
+    Node new_node = new Node(new_data);
+    if (head == null) {
+      head = new_node;
+    } else {
+      new_node.setNextRef(head);
+      head = new_node;
+    }
+
+  }
+
 }
