368dc765c30ea5fc87573f8b1f278006654397cf
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-06 06:21:59 +1400

    All methods are invoked in this class

--- src/QueueImplementation/Main.java
+++ src/QueueImplementation/Main.java
@@ -3,5 +3,26 @@ package QueueImplementation;
 public class Main {
     public static void main(String[] args) {
         QueueWithArray queueWithArray = new QueueWithArray(4);
+
+        queueWithArray.enqueue(18);
+        queueWithArray.enqueue(22);
+        queueWithArray.enqueue(25);
+        queueWithArray.enqueue(45);
+        queueWithArray.enqueue(65);
+
+        System.out.println("Queue is: ");
+        queueWithArray.displayQueue();
+
+        queueWithArray.dequeue();
+        System.out.println("Now after dequeue Queue is :");
+        queueWithArray.displayQueue();
+
+        System.out.println("peekInQueue operation : Check whether an element is at top or not");
+        queueWithArray.peekInQueue();
+
+        System.out.println("Delete Operation is :");
+        queueWithArray.deleteQueue();
+        queueWithArray.displayQueue();
+
     }
 }
