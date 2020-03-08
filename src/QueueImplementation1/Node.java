aaa00daf3b6aef7aa750c45ac5e8a6c20942950c
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-08 22:35:34 +1400

    This is Node class having data and next Reference

--- src/QueueImplementation1/Node.java
+++ src/QueueImplementation1/Node.java
@@ -7,6 +7,22 @@ public class Node {
  public Node(int data, Node next) {
   this.data = data;
   this.next = null;
-
  }
+
+ public int getData() {
+  return data;
+ }
+
+ public void setData(int data) {
+  this.data = data;
+ }
+
+ public Node getNext() {
+  return next;
+ }
+
+ public void setNext(Node next) {
+  this.next = next;
+ }
+
 }
