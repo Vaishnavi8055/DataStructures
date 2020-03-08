a053ca1efe67e872049c545d83a1dac013d554f9
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-09 05:47:38 +1400

    enqueue method called

--- src/QueueImplementation1/Main.java
+++ src/QueueImplementation1/Main.java
@@ -1,5 +1,17 @@
 package QueueImplementation1;
 
 public class Main {
+    public static void main(String[] args) {
+        QueueUsingLinkedList queueUsingLinkedList=new QueueUsingLinkedList();
+        System.out.println("Queue is :");
+        queueUsingLinkedList.Enqueue(98);
+        queueUsingLinkedList.Enqueue(45);
+        queueUsingLinkedList.Enqueue(65);
+        queueUsingLinkedList.Enqueue(67);
+        queueUsingLinkedList.display();
 
+
+
+
+    }
 }
